9a47f78591baef1fa6ac19e7d0e7e1e32dce61a5
RenChu Wang <patrick1031wang@gmail.com>
2026-02-10 21:00:54 -0800

    ok

--- ci/build.py
+++ ci/build.py
@@ -1,22 +1,17 @@
 # Copyright (c) RenChu Wang - All Rights Reserved
 
-from argparse import ArgumentParser
-
 import gha
 import pdm
 import sh
 
 
-def main(cfg: str):
+def main():
     gha.setup()
     pdm.install()
 
     with sh.run_in_root():
-        pdm.run(f"python -m cv {cfg}")
+        pdm.run(f"python main.py")
 
 
 if __name__ == "__main__":
-    parser = ArgumentParser()
-    parser.add_argument("config")
-    flags = vars(parser.parse_args())
-    main(flags["config"])
+    main()
---
+++ main.py
@@ -0,0 +1,43 @@
+# Copyright (c) RenChu Wang - All Rights Reserved
+
+import shutil
+from argparse import ArgumentParser
+from pathlib import Path
+
+import rich
+from omegaconf import OmegaConf
+
+from cv import Section, files, resumes
+
+
+def main(cfg: Path) -> None:
+    assert cfg.exists() and cfg.is_file()
+    flags = dict(OmegaConf.load(cfg))
+
+    generate_resume(flags["sections"])
+
+    files.BUILD.mkdir(exist_ok=True)
+    _ = shutil.copy2(files.TEMPLATE / "resume.css", files.BUILD)
+
+
+def generate_resume(sections: list[str]):
+    out = resumes.resume(Section(sec) for sec in sections)
+    tee(out, file="index.html")
+
+
+def tee(out: str, file: str) -> None:
+    rich.print(out)
+    with (files.BUILD / file).open("w+") as f:
+        _ = f.write(out)
+
+
+if __name__ == "__main__":
+    CWD = Path(__file__).parent
+    parser = ArgumentParser()
+    _ = parser.add_argument(
+        "--cfg",
+        type=Path,
+        default=CWD / "config.yaml",
+    )
+    flags = vars(parser.parse_args())
+    main(**flags)
