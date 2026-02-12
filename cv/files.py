b0c3e034ab01bb0f3818634752b879f6f04e9258
RenChu Wang <patrick1031wang@gmail.com>
2026-02-11 23:51:51 -0800

    positions

--- cv/files.py
+++ cv/files.py
@@ -11,7 +11,13 @@ from jinja2 import Environment, FileSystemLoader, Template, meta
 from markdown_it import MarkdownIt
 from omegaconf import OmegaConf
 
-__all__ = ["ROOT", "get_template", "find_template_vars", "get_data"]
+__all__ = [
+    "ROOT",
+    "get_template",
+    "find_template_vars",
+    "get_data",
+    "MarkdownRender",
+]
 
 ROOT = Path(__file__).parent.parent.absolute()
 "Project root."
@@ -26,12 +32,11 @@ BUILD = ROOT / "build"
 "The build directory."
 
 
-class _Rendered(str):
-    "A 'tag' s.t. we know that the item is already processed. ``str`` subclass."
-
-
 @dcls.dataclass
 class MarkdownRender:
+    class IsRendered(str):
+        "A 'tag' s.t. we know that the item is already processed. ``str`` subclass."
+
     template: Template
 
     def render(self, **kwargs):
@@ -40,17 +45,16 @@ class MarkdownRender:
 
     @typing.no_type_check
     def _render(self, obj: Any) -> Any:
-        match obj:
+        # Skip any rendering if it's already previously rendered.
+        if isinstance(obj, self.IsRendered):
+            return obj
 
+        match obj:
             case None | int() | float() | bool():
                 return obj
 
-            # Skip any rendering if it's already previously rendered.
-            case _Rendered(item):
-                return item
-
             case str():
-                return _Rendered(self._md.renderInline(obj))
+                return self.IsRendered(self._md.renderInline(obj))
 
             case Mapping():
                 return {key: self._render(val) for key, val in obj.items()}
@@ -59,8 +63,11 @@ class MarkdownRender:
                 return [self._render(val) for val in obj]
 
             # Do not recurse into custom objects.
-            case _:
+            case object():
                 return obj
+
+            case _:
+                raise TypeError(type(obj))
 
     @functools.cached_property
     def _md(self):
