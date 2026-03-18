95c592d70d7ae83ca07fff145e75674e4f7c020b
RenChu Wang <patrick1031wang@gmail.com>
2026-03-18 03:46:17 -0700

    done

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Copyright (c) RenChu Wang - All Rights Reserved
 
+import contextlib as ctxl
 import dataclasses as dcls
 import shutil
 import typing
@@ -11,10 +12,25 @@ from omegaconf import OmegaConf
 from cv import Section, files, resumes
 
 
+@ctxl.contextmanager
+def log_stage(message: str):
+
+    try:
+        print(message, end="... ")
+        yield
+    except Exception as e:
+        print(f"Failed with error: {e}")
+
+    print("Done.")
+
+
 @dcls.dataclass(frozen=True)
 class GenerationConfig:
     sections: list[str]
     keywords: list[str]
+
+    def generate(self):
+        return generate_resume(self)
 
 
 @typing.no_type_check
@@ -28,19 +44,26 @@ def main(cfg: Path) -> None:
         gitignore.write_text("*")
 
     # Copy CSS.
-    _ = shutil.copy2(files.TEMPLATE / "resume.css", files.BUILD)
+    with log_stage(f"Copy CSS to template: {files.TEMPLATE}"):
+        _ = shutil.copy2(files.TEMPLATE / "resume.css", files.BUILD)
 
-    gc = GenerationConfig(
+    cfg = GenerationConfig(
         sections=flags["sections"],
         keywords=flags["keywords"],
     )
-    generate_resume(gc)
+    cfg.generate()
 
 
 def generate_resume(cfg: GenerationConfig, /):
     sections = [Section(cfg=sec, keywords=cfg.keywords) for sec in cfg.sections]
-    out = resumes.resume(sections=sections)
-    with (files.BUILD / "index.html").open("w+") as f:
+
+    with log_stage("Generating resume"):
+        out = resumes.resume(sections=sections)
+
+    with (
+        log_stage(f"Write file to index.html in build: {files.BUILD}"),
+        (files.BUILD / "index.html").open("w+") as f,
+    ):
         _ = f.write(out)
 
 
