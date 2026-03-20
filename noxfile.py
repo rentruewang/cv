8dc0ec0b630541f6613a4e26d426dd6a4227fe25
RenChu Wang <patrick1031wang@gmail.com>
2026-03-19 20:10:35 -0700

    🎎 Switch to using nox (#14)

---
+++ noxfile.py
@@ -0,0 +1,163 @@
+# Copyright (c) RenChu Wang - All Rights Reserved
+
+import dataclasses as dcls
+import functools
+import os
+import sys
+
+import nox
+from nox import Session
+
+
+@nox.session
+def build(session: Session):
+    "Nox `build` command. Calls `python main.py`."
+
+    pdm(session).run("python", "main.py")
+
+
+@nox.session
+def pre_commit(session: Session):
+    "Runs the pre-commit commands."
+
+    formatting(session)
+    typing(session)
+
+
+@nox.session
+def formatting(session: Session):
+    "Nox `formatting` command. Calls `autoflake`, `isort`, `black`, in that order."
+    autoflake(session)
+    isort(session)
+    black(session)
+
+
+@nox.session
+def autoflake(session: Session):
+    "Nox `autoflake` command. Calls `autoflake` command."
+    pdm(session).run("autoflake", ".")
+
+
+@nox.session
+def isort(session: Session):
+    "Nox `isort` command. Calls `isort` command."
+    pdm(session).run("isort", ".")
+
+
+@nox.session
+def black(session: Session):
+    "Nox `black` command. Calls `black` command."
+    pdm(session).run("black", ".")
+
+
+@nox.session
+def mypy(session: Session):
+    "Nox `mypy` command. Calls `mypy` command."
+    pdm(session).run("mypy", ".")
+
+
+@nox.session
+def typing(session: Session):
+    "Nox `typing` command. Calls `mypy` command."
+    mypy(session)
+
+
+@functools.cache
+def github(session: Session):
+    "Global singleton of `github`."
+    return _Github(session)
+
+
+@functools.cache
+def pdm(session: Session):
+    "Global singleton of `pdm`."
+    return _Pdm(session)
+
+
+@dcls.dataclass(frozen=True)
+class _Github:
+    "The manager for setting up github."
+
+    session: Session
+    "The nox session to use."
+
+    @functools.cache
+    def setup(self) -> None:
+        "The shared entrypoint to GitHub Actions scripts"
+
+        # Does nothing outside of GitHub Actions.
+        if not self.active():
+            return
+
+        self._remove_unwanted_files()
+        self._log_storage_usage()
+
+    def _run(self, *args: str):
+        self.session.run_install(*args, external=True)
+
+    def _remove_unwanted_files(self) -> None:
+        "Remove the files GitHub Actions pre-installed."
+
+        print("Removing files we did not ask for...")
+
+        for folder in [
+            "/usr/local/lib/android",
+            "/usr/share/dotnet",
+            "/usr/local/.ghcup",
+        ]:
+            self._run("sudo", "rm", "-rf", folder)
+
+        self._run("docker", "system", "prune", "-af", "--volumes")
+
+    def _log_storage_usage(self) -> None:
+        "Log how much usage is currently being used by GitHub Actions."
+        print("Investigating how much storage is used in GitHub Actions...")
+
+        self._run("df", "-h")
+
+    @staticmethod
+    def active() -> bool:
+        "Detect whether or not it is running in GitHub Actions."
+
+        print("Checking if we are in GitHub Actions...", end=" ")
+        result = os.getenv("GITHUB_ACTIONS") == "true"
+        print("Yes" if result else "No")
+        return result
+
+
+@dcls.dataclass(frozen=True)
+class _Pdm:
+    session: Session
+
+    def __post_init__(self):
+        github(self.session).setup()
+
+        if _is_remote(self.session):
+            self._run("pdm", "config", "python.use_venv", "true")
+
+    def sync(self) -> None:
+        self._sync_or_install("sync")
+
+    def install(self):
+        self._sync_or_install("install")
+
+    def _install_and_run(self, *args: str):
+        self.install()
+        self._run(*args)
+
+    def run(self, *args: str):
+        self._install_and_run("pdm", "run", *args)
+
+    def _sync_or_install(self, mode: str) -> None:
+        # Don't repeatedly reinstall locally.
+        if not _is_remote(self.session):
+            return
+
+        self.session.run_install("pdm", mode, "-G:all")
+
+    def _run(self, *args: str):
+        self.session.run(*args, external=True)
+
+
+def _is_remote(session: Session):
+    return github(session).active()
